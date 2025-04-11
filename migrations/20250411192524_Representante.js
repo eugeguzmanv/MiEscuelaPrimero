a3aef4165e07c3f112f9864016e8783c17534d58
Diego-ACC <A01645484@tec.mx>
2025-04-11 17:49:21 -0600

    BACKEND: Primera configuracion de tabla (Representantes)

--- migrations/20250411192524_Representante.js
+++ migrations/20250411192524_Representante.js
@@ -1,15 +1,18 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('Representante', (table) => {
+        table.increments('idRepresentante').primary();
+        table.string('correo_electronico').notNullable();
+        table.string('nombre').notNullable();
+        table.string('contrasena').notNullable();
+        table.string('numero_telefonico').notNullable();
+        table.string('CCT').foreignKey('CCT').references('CCT').inTable('Escuela'); //Foreing key de la tabla Escuela, (Revisar tipo de dato a ingresar)
+        table.string('rol').notNullable();
+        table.integer('anios_experiencia').notNullable();
+        table.string('proximo_a_jubilarse').notNullable(); //Revisar tipo de dato a ingresar
+        table.string('cambio_zona').notNullable(); //Revisar tipo de dato a ingresar
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('Representante');
 };
