54fa546c2bea7c9274e9c44ffa4af0a34b9a3a9f
Diego-ACC <A01645484@tec.mx>
2025-04-14 17:15:08 -0600

    BACKEND: Renombrando archivos de migraciones cambio #3

--- migrations/8_Escuela_Recibe_Apoyo.js
+++ migrations/8_Escuela_Recibe_Apoyo.js
@@ -1,7 +1,9 @@
 exports.up = function(knex) {
     return knex.schema.createTable('Escuela_Recibe_Apoyo', (table) => {
         table.increments('idSolicitud').primary();
+        table.string('CCT').notNullable(); //CCT de la escuela que recibe el apoyo
         table.foreign('CCT').references('CCT').inTable('Escuela'); //Foreing key de la tabla Escuela
+        table.integer('idApoyo').notNullable(); //idApoyo del apoyo que recibe la escuela
         table.foreign('idApoyo').references('idApoyo').inTable('Apoyo'); //Foreing key de la tabla Apoyo
     });
 };
